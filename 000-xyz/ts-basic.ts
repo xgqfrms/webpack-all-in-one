4bd320e96b7d9e9ff30c6c904a0aae6bbc20abb9
xgqfrms <xgqfrms@outlook.com>
2019-11-23 23:59:23 +0800

    Create ts-basic.ts

---
+++ 000-xyz/ts-basic.ts
@@ -0,0 +1,11 @@
+// const log = console.log;
+
+const any: any = 35;
+
+const multi: boolean | number = 35;
+
+let example1: boolean = true;
+
+let example2: number = 35;
+
+let example3: string = 'Hello world';
