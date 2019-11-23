37b17b06b16a7a8110b9ceb6c1c499d92ba78d85
xgqfrms <xgqfrms@outlook.com>
2019-11-23 22:28:24 +0800

    Update arrow-this.js

--- 000-xyz/arrow-this.js
+++ 000-xyz/arrow-this.js
@@ -1,5 +1,6 @@
 const log = console.log;
 
+// 1. constructor bug
 const func = () => {
     this.name = `xgqfrms`;
     title = `arrow function`;
@@ -10,15 +11,17 @@ log(`\nfunc`, func);
 log(`\nfunc.prototype`, func.prototype);
 // func.prototype undefined
 
-// func.prototype.print = function () {
-//     let name  = this.name;
-//     log(`\nname =`, name);
-// };
+// 2. prototype bug
+
+func.prototype.print = function () {
+    let name  = this.name;
+    log(`\nname =`, name);
+};
 // TypeError: Cannot set property 'print' of undefined
 
-const obj = {
-    name: "webgeeker",
-};
+// const obj = {
+//     name: "webgeeker",
+// };
 
 // log(`\nfunc =`, func);
 // log(`\nfunc.print =`, func.print);
