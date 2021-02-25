e4631768ee1972b0580e8ce61eaef40d2d2915ea
xgqfrms <xgqfrms@outlook.com>
2021-02-25 22:25:21 +0800

    Update vue-3.x.js

--- webpack-4.x-console.log/src/vue-3.x.js
+++ webpack-4.x-console.log/src/vue-3.x.js
@@ -13,4 +13,34 @@ export default {
             evtHandler,
         };
     },
+    // setup() {
+    //     const a = ref(1);
+    //     const b = computed(() => a.value * 2);
+    //     return {
+    //         a,
+    //         b,
+    //     };
+    // },
+    // setup() {
+    //     const site = ref('xgqfrms.xyz');
+    //     watchEffect(() => {
+    //         console.log(site.value);
+    //     });
+    //     return {
+    //         site,
+    //     };
+    // },
+    // template
+    // <div ref='example'> Example Div </div>
+    // script
+    // setup() {
+    //     const example = ref('xgqfrms.xyz');
+    //     // wait for DOM to mount
+    //     onMounted(() => {
+    //         console.log(example.value);
+    //     });
+    //     return {
+    //         example,
+    //     };
+    // },
 };
